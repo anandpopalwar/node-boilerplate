74bc578d53ad1322dac673d358be929c15ac88b7
anandpopalwar <anandpopalwar444@gmail.com>
2023-02-24 16:21:42 +0530

    1st commit of nodeusepublic

--- app.js
+++ app.js
@@ -2,13 +2,23 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 const ejs = require('ejs');
-const { urlencoded } = require('body-parser');
 app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine','ejs')
+app.use(express.static('public'))
+const users=[]
 
-app.get('',((req,res)=>{
-    res.send("<h1>home page</h1>")
+app.get('/',((req,res)=>{
+    res.render('home')
+    console.log('home page is connected....')
 }));
+app.post('/user',((req,res)=>{
+    let user={name:req.body.name}
+    users.push(user)
+    res.render('user',{student:users})
+   console.log('user page is connected....')
+   res.redirect('/')
+}))   
+
 
 app.listen(3000,(()=>{
     console.log('server is connected....')
